9d1afd7cbd2e71b62bd55a3bfc62f7c1525ca65d
MatiT88 <matiastecici@gmail.com>
2023-03-02 11:28:15 -0300

    rutas y otros

--- src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Heroe } from '../../interfaces/heroes.interface';
-import { ImagenPipe } from '../../pipes/imagen.pipe';
+
 
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html',
-  styles: [
+  styles: [` 
+    mat-card{
+      margin-top: 20px;
+  }`
   ]
 })
 export class HeroeTarjetaComponent {
