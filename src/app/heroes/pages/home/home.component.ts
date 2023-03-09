c46c9866f2efc29db5b17616b853eb1b423050eb
MatiT88 <matiastecici@gmail.com>
2023-03-09 11:46:00 -0300

    configurando guards

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +14,16 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  get auth() {
+    return this.authService.auth;
+  }
+
+  constructor(private router: Router,
+              private authService: AuthService) {}
+
+  logout() {
+    //ir al backend
+    //usuario
+    this.router.navigate(['./auth'])
+  }
 }
